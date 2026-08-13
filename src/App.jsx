8a27bec9d301b818ec21a26a35814482319387b4
Overlander <anuragnandal@hotmail.com>
2026-08-13 11:04:12 -0400

    refactor: extract routes to separate file and folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router'
-import Shop from './components/Shop'
-import Cart from './components/Cart'
-import Navbar from './components/Navbar'
+import routes from './routes/routes'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <Navbar />
-        <h2>Home page</h2>
-      </div>)
-  },
-  {
-    path: "/shop",
-    element: (
-      <div>
-        <Navbar />
-        <Shop />
-      </div>)
-  },
-  {
-    path: "/cart",
-    element: (
-      <div>
-        <Navbar />
-        <Cart />
-      </div>
-    )
-  }
-])
+const router = createBrowserRouter(routes)
 
 function App() {
   return (
---
+++ src/routes/routes.jsx
@@ -0,0 +1,33 @@
+import Navbar from '../components/Navbar'
+import Shop from '../components/Shop'
+import Cart from '../components/Cart'
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <h2>Home page</h2>
+      </div>)
+  },
+  {
+    path: "/shop",
+    element: (
+      <div>
+        <Navbar />
+        <Shop />
+      </div>)
+  },
+  {
+    path: "/cart",
+    element: (
+      <div>
+        <Navbar />
+        <Cart />
+      </div>
+    )
+  }
+]
+
+export default routes;
