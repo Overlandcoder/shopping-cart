9db1376dac9e8fdec732e57cdbaf09dffadb8031
Overlander <anuragnandal@hotmail.com>
2026-08-13 12:55:59 -0400

    feat: fetch and render product titles from FakeStoreAPI on Shop page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
+
 function Shop() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=10')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
   return(
-    <>
-      Welcome to the shop
-    </>
+    <div>
+      <h2>Shop</h2>
+      <ul>
+        {products.map(product => {
+          return <li>{product.title}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
