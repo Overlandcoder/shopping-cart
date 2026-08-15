135bc00643ab98982e653d9462cd49ba8b8986f8
Overlander <anuragnandal@hotmail.com>
2026-08-15 10:30:26 -0400

    feat: implement nested route layout using RootLayout and Outlet

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,34 +1,26 @@
-import Navbar from '../components/Navbar';
+import RootLayout from '../components/RootLayout'
+import Home from '../components/Home'
 import Shop from '../components/Shop';
 import Cart from '../components/Cart';
 
 const routes = [
   {
     path: '/',
-    element: (
-      <div>
-        <Navbar />
-        <h2>Home page</h2>
-      </div>
-    ),
-  },
-  {
-    path: '/shop',
-    element: (
-      <div>
-        <Navbar />
-        <Shop />
-      </div>
-    ),
-  },
-  {
-    path: '/cart',
-    element: (
-      <div>
-        <Navbar />
-        <Cart />
-      </div>
-    ),
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "shop",
+        element: <Shop />
+      },
+      {
+        path: "cart",
+        element: <Cart />
+      }
+    ]
   },
 ];
 
